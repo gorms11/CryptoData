e92f15fc26f4a273cee1a370ccf3ef428a2d8065
gorms <jamesemail11@gmail.com>
2018-01-11 21:45:58 -0600

    TEST

--- current_coin_data.py
+++ current_coin_data.py
@@ -1,67 +1,70 @@
-#Panadas is the only library not included by default: pip install pandas
-
 import json
 import os
 import pandas as pd
 import urllib.request
+import datetime
 
 
 def JSONDictToDF(d):
-	'''
-	Converts a dictionary created from json.loads to a pandas dataframe
-	d:      The dictionary
-	'''
-	length = len(d)
-	cols = []
-	if length > 0:  # Place the column in sorted order
-		cols = sorted(list(d[0].keys()))
-	df = pd.DataFrame(columns=cols, index=range(length))
-	for i in range(length):
-		for coli in cols:
-			df.set_value(i, coli, d[i][coli])
-	return df
+    '''
+    Converts a dictionary created from json.loads to a pandas dataframe
+    d:      The dictionary
+    '''
+    length = len(d)
+    cols = []
+    if length > 0:  # Place the column in sorted order
+        cols = sorted(list(d[0].keys()))
+    df = pd.DataFrame(columns=cols, index=range(length))
+    for i in range(length):
+        for coli in cols:
+            df.set_value(i, coli, d[i][coli])
+    return df
 
 
 def GetAPIUrl(cur):
-	'''
-	Makes a URL for querying historical prices of a cyrpto from Poloniex
-	cur:    3 letter abbreviation for cryptocurrency (BTC, LTC, etc)
-	'''
-	url = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=' + cur + '&tsyms=USD'
-	return url
+    '''
+    Makes a URL for querying historical prices of a cyrpto from Poloniex
+    cur:    3 letter abbreviation for cryptocurrency (BTC, LTC, etc)
+    '''
+    url = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=' + cur + '&tsyms=USD'
+    return url
 
 
 def GetCurDF(cur, fp):
-	'''
-	cur:    3 letter abbreviation for cryptocurrency (BTC, LTC, etc)
-	fp:     File path (to save price data to CSV)
-	'''
-	openUrl = urllib.request.urlopen(GetAPIUrl(cur))
-	web_data = openUrl.read()
-	openUrl.close()
-	json_web_data = json.loads(web_data)
-	string_json_web_data = str(json_web_data["RAW"][cur]["USD"])
-	string_json_web_data = '[' + string_json_web_data + ']'
+    '''
+    cur:    3 letter abbreviation for cryptocurrency (BTC, LTC, etc)
+    fp:     File path (to save price data to CSV)
+    '''
+    openUrl = urllib.request.urlopen(GetAPIUrl(cur))
+    web_data = openUrl.read()
+    openUrl.close()
+    json_web_data = json.loads(web_data)
+    timestamp = str(datetime.datetime.fromtimestamp(json_web_data["RAW"][cur]["USD"]['LASTUPDATE']))
+    json_web_data["RAW"][cur]["USD"]['LASTUPDATE'] = timestamp
+    string_json_web_data = str(json_web_data["RAW"][cur]["USD"])
+    string_json_web_data = '[' + string_json_web_data + ']'
+    print(string_json_web_data)
+    modified_json_web_data = json.loads(string_json_web_data.replace("\'", "\""))
 
 
-	modified_json_web_data = json.loads(string_json_web_data.replace("\'", "\""))
-	dict_json_web_data = JSONDictToDF(modified_json_web_data)
-	dict_json_web_data.to_csv(fp, sep=',')
-	return dict_json_web_data  # %%Path to store cached currency data
+
+    dict_json_web_data = JSONDictToDF(modified_json_web_data)
+    dict_json_web_data.to_csv(fp, sep=',')
+    return dict_json_web_data  # %%Path to store cached currency data
 
 
 datPath = 'CurDat/'
 if not os.path.exists(datPath):
-	os.mkdir(datPath)
+    os.mkdir(datPath)
 # Different cryptocurrency types
 coin_type = ['ADA', 'LTC', 'ETH', 'XMR', 'XVG', 'XLM', 'ZEC']
 
 # Store data frames for each of above types
 D = []
 for coin in coin_type:
-	dfp = os.path.join(datPath, coin + '.csv')
-	df = GetCurDF(coin, dfp)
-	D.append(df)
+    dfp = os.path.join(datPath, coin + '.csv')
+    df = GetCurDF(coin, dfp)
+    D.append(df)
 
 print(D)
 '''
@@ -74,5 +77,5 @@ print(D)
 cr = min(Di.shape[0] for Di in D)
 for i in range(len(cl)):
     D[i] = D[i][(D[i].shape[0] - cr):]
-    
+
 '''
