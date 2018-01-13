193aefbb177398eea8099da393ab279e7b6b85cd
bob_johnson <tythatsk8s05@yahoo.com>
2018-01-12 18:00:17 -0600

    GUI now does linux/windows. need to add os detection.

--- current_coin_data.py
+++ current_coin_data.py
@@ -3,6 +3,9 @@ import os
 import pandas as pd
 import urllib.request
 import datetime
+import tkinter as tk
+from time import sleep
+
 
 def JSONDictToDF(d):
     '''
@@ -44,20 +47,54 @@ def GetCurDF(cur, fp):
     string_json_web_data = '[' + string_json_web_data + ']'
     print(string_json_web_data)
     modified_json_web_data = json.loads(string_json_web_data.replace("\'", "\""))
-
-
-
     dict_json_web_data = JSONDictToDF(modified_json_web_data)
     dict_json_web_data.to_csv(fp, sep=',')
     return dict_json_web_data  # %%Path to store cached currency data
+
+def shift(text):
+	sleep(1)
+	text = text[1:] + text[0]
+	display_text.set(text)
+	master.after(100,shift(text))
+
+#Below block is for GUI Ticker
+#display_text = variable for text
+#msg = label that holds text
+#master = main tkinter window
+master = tk.Tk()
+#master.wm_attributes('-type', 'splash','-topmost',1) uncomment for linux
+master.geometry('1920x20+0+0')
+
+master.wm_attributes('-topmost',1)
+master.overrideredirect(1) #comment out if using linux
+
+text = "hello fagget"
+display_text = tk.StringVar()
+msg = tk.Label(master, textvariable = display_text,height='20',width='1920')
+msg.config(bg='black', font=('times', 12),fg='white')
+msg.pack()
+master.lift()
+display_text.set(text)
+
+while True:
+    sleep(1)
+    text = text[1:]+text[0]
+    display_text.set(text)
+    master.lift()
+    master.focus()
+    master.overrideredirect(1)
+    master.update()
+
+
+
 
 
 datPath = 'CurDat/'
 if not os.path.exists(datPath):
     os.mkdir(datPath)
 # Different cryptocurrency types
-# need to add TRX
-coin_type = ['ADA', 'LTC', 'ETH', 'XMR', 'XVG', 'XLM', 'ZEC']
+
+coin_type = ['ADA', 'LTC', 'ETH', 'XMR', 'XVG', 'XLM', 'ZEC', 'TRX']
 
 # Store data frames for each of above types
 D = []
@@ -67,6 +104,8 @@ for coin in coin_type:
     D.append(df)
 
 print(D)
+
+
 '''
     try:
         df = pd.read_csv(dfp, sep=',')
