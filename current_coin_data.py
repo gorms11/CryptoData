296575c41c765f0ab0b0000b291e2d164d1b8ca8
gorms <jamesemail11@gmail.com>
2018-02-20 22:14:26 -0600

    Each coin now changes color based on change in value green = value went up red = value went down white = value stayed the same

--- current_coin_data.py
+++ current_coin_data.py
@@ -10,7 +10,7 @@ from time import sleep
 import platform
 import threading
 import sqlite3
-
+from tkinter import*
 
 
 
@@ -86,59 +86,86 @@ def DataGrabber_NoCSV():
 	'''Grabs all coin data from API WITHOUT making .CSV files. Runs every 18 seconds
 	   Updates the string displayed in the user interface
 	   Stores current and previous values of all coins in 2D array for easy comparison'''
-	threading.Timer(18.0, DataGrabber_NoCSV).start()
+
+	#threading.Timer(18.0, DataGrabber_NoCSV).start()
 	global CoinList
 	global coin_data_copy
-	list_of_coin_data = []
-	for coin in coin_type:
-		text = GetCur_NoCSV(coin)
-		list_of_coin_data.append(text)
-	print('API Call Executed!')
-	CoinList = list_of_coin_data
-	#return (list_of_coin_data)
+	global color
+	while True:
+		list_of_coin_data = []
+		for coin in coin_type:
+			text = GetCur_NoCSV(coin)
+			list_of_coin_data.append(text)
+		print('API Call Executed!')
+		CoinList = list_of_coin_data
+		# return (list_of_coin_data)
 
-	coin_fiat_data = []
-	for x in range(len(list_of_coin_data)):
-		text = list_of_coin_data[x]
-		text = str(text['RAW'][coin_type[x]]['USD']['PRICE'])
-		compare_list[x][1] = compare_list[x][0]
-		compare_list[x][0] = float(text)
-		text = coin_type[x] + ' :' + ' $' + text + '    '
-		coin_fiat_data.append(text)
-	coin_data_copy = coin_fiat_data
-	print(compare_list)
+		for x in range(len(list_of_coin_data)):
+			text = list_of_coin_data[x]
+			text = str(text['RAW'][coin_type[x]]['USD']['PRICE'])
+			compare_list[x][1] = compare_list[x][0]
+			compare_list[x][0] = float(text)
+		#	text2 = ' ' + coin_type[x] + ' :' + ' $' + text + '    '
+
+			if compare_list[x][0] > compare_list[x][1]:
+				display_number_green[x].set(" ")
+				display_number_red[x].set(" ")
+				display_number_white[x].set(" ")
+				display_number_green[x].set("  " + coin_type[x] + ' :' + ' $' + text)
+
+			if compare_list[x][0] < compare_list[x][1]:
+				display_number_green[x].set(" ")
+				display_number_red[x].set(" ")
+				display_number_white[x].set(" ")
+				display_number_red[x].set("  "  + coin_type[x] + ' :' + ' $' + text)
+
+			if compare_list[x][0] == compare_list[x][1]:
+				display_number_green[x].set(" ")
+				display_number_red[x].set(" ")
+				display_number_white[x].set(" ")
+				display_number_white[x].set("  " + coin_type[x] + ' :' + ' $' + text)
+
+
+		print(compare_list)
+
+		sleep(10)
+
+
+
+
 
 
 def WriteToDB():
 	'''records data for each coin in its own table for local SQL database'''
 	threading.Timer(150.0, WriteToDB).start()
+
 	print('writing to database!')
 	db = sqlite3.connect('CoinData.db')
 	c = db.cursor()
 
 	for x in range(len(CoinList)):
-		dbList= CoinList[x]
+		dbList = CoinList[x]
 		timestamp = str(datetime.datetime.fromtimestamp(dbList["RAW"][coin_type[x]]["USD"]['LASTUPDATE']))
 		dbList["RAW"][coin_type[x]]["USD"]["LASTUPDATE"] = timestamp
 
 		c.execute('''CREATE TABLE IF NOT EXISTS {tn}(`PRICE`          INTEGER,
-													`LASTVOLUME`      INTEGER,
-													`LASTVOLUMETO`	  INTEGER,
-													`VOLUMEDAY`	      INTEGER,
-													`VOLUMEDAYTO`	  INTEGER,
-													`VOLUME24HOUR`	  INTEGER,
-													`VOLUME24HOURTO`  INTEGER,
-													`HIGH24HOUR`	  INTEGER,
-													`LOW24HOUR`	      INTEGER,
-													`MKTCAP`	      INTEGER,
-													`SUPPLY`	      INTEGER,
-													`TOTALVOLUME24H`  INTEGER,
-													`TOTALVOLUME24HTO`INTEGER,
-													`LASTUPDATE`	  INTEGER)'''.format(tn=coin_type[x]))
+		    													`LASTVOLUME`      INTEGER,
+		    													`LASTVOLUMETO`	  INTEGER,
+		    													`VOLUMEDAY`	      INTEGER,
+		    													`VOLUMEDAYTO`	  INTEGER,
+		    													`VOLUME24HOUR`	  INTEGER,
+		    													`VOLUME24HOURTO`  INTEGER,
+		    													`HIGH24HOUR`	  INTEGER,
+		    													`LOW24HOUR`	      INTEGER,
+		    													`MKTCAP`	      INTEGER,
+		    													`SUPPLY`	      INTEGER,
+		    													`TOTALVOLUME24H`  INTEGER,
+		    													`TOTALVOLUME24HTO`INTEGER,
+		    													`LASTUPDATE`	  INTEGER)'''.format(tn=coin_type[x]))
 
 		c.execute('''INSERT INTO {tn}(PRICE, LASTVOLUME, LASTVOLUMETO, VOLUMEDAY, VOLUMEDAYTO, VOLUME24HOUR, VOLUME24HOURTO,
-		                             HIGH24HOUR, LOW24HOUR, MKTCAP, SUPPLY, TOTALVOLUME24H, TOTALVOLUME24HTO, LASTUPDATE)
-		             VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?)'''.format(tn=coin_type[x]),
+		    		                             HIGH24HOUR, LOW24HOUR, MKTCAP, SUPPLY, TOTALVOLUME24H, TOTALVOLUME24HTO, LASTUPDATE)
+		    		             VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?)'''.format(tn=coin_type[x]),
 				  (str(dbList["RAW"][coin_type[x]]["USD"]["PRICE"]),
 				   str(dbList["RAW"][coin_type[x]]["USD"]["LASTVOLUME"]),
 				   str(dbList["RAW"][coin_type[x]]["USD"]["LASTVOLUMETO"]),
@@ -154,10 +181,17 @@ def WriteToDB():
 				   str(dbList["RAW"][coin_type[x]]["USD"]["TOTALVOLUME24HTO"]),
 				   str(dbList["RAW"][coin_type[x]]["USD"]["LASTUPDATE"])))
 		db.commit()
-		print('wrote to ', [coin_type[x]] , ' database!')
+		print('wrote to ', [coin_type[x]], ' database!')
 
 	db.close()
 	print('done writing to database!')
+
+
+
+
+
+	# sleep(1.0)
+	# CoinList = DataGrabber_NoCSV()
 
 
 
@@ -166,23 +200,28 @@ def WriteToDB():
 # msg = label that holds text
 # master = main tkinter window
 
-master = tk.Tk()
-master.geometry('1920x20+0+0')
 
+
+
+#root.geometry('1920x20+0+0')
+'''
 # if linux
 if platform.system() is 'Linux':
-	master.wm_attributes('-type', 'splash', '-topmost', 1)
+	root.wm_attributes('-type', 'splash', '-topmost', 1)
 
 # if windows
 if platform.system() is 'Windows':
-	master.wm_attributes('-topmost', 1)
-	master.overrideredirect(1)
+	root.wm_attributes('-topmost', 1)
+	root.overrideredirect(1)
 
-display_text = tk.StringVar()
-msg = tk.Label(master, textvariable=display_text, height='20', width='1920', anchor='w')
-msg.config(bg='black', font=('times', 12), fg='white')
-msg.pack()
-master.lift()
+
+
+'''
+
+root=Tk()
+
+
+
 
 
 coin_type = ['LTC', 'ETH', 'XMR', 'XVG', 'XLM', 'ZEC', 'XRP', 'REQ', 'BCH', 'LINK', 'NXT', 'BTC']
@@ -191,30 +230,80 @@ if not os.path.exists(datPath):
 	os.mkdir(datPath)
 
 
-#provides something to display when program first starts
-display_text.set('grabbing data...please wait')
-master.lift()
-master.focus()
-master.update()
+#root.wm_attributes('-type', 'splash', '-topmost', 1)
+
+
+
+display_number_white = []
+display_number_green = []
+display_number_red = []
+
+
+for i in range (len(coin_type)):
+	display_number_white.append(i)
+	display_number_white[i] = StringVar()
+	Label(root, textvariable=display_number_white[i], bg='black', font=('times', 12), fg ='white').grid(row=0, column=i, sticky=tk.W, padx=4)
+
+
+
+for i in range (len(coin_type)):
+	display_number_green.append(i)
+	display_number_green[i] = StringVar()
+	Label(root, textvariable=display_number_green[i], bg='black', font=('times', 12), fg ='green').grid(row=0, column=i, sticky=tk.W, padx=4)
+
+
+for i in range (len(coin_type)):
+	display_number_red.append(i)
+	display_number_red[i] = StringVar()
+	Label(root, textvariable=display_number_red[i], bg='black', font=('times', 12), fg ='red').grid(row=0, column=i, sticky=tk.W, padx=4)
+
+
+root.config(bg='black')
+
+
+
+
+
 
 #2D array for comparing current value to previous value for each coin
-compare_list = [[None for x in range(2)] for y in range(len(coin_type))]
+compare_list = [[float(0.0) for x in range(2)] for y in range(len(coin_type))]
 
 CoinList = []
 coin_data_copy = []
 
-#Initializes Datagrabber_NoCSV and WriteToDB threads
-DataGrabber_NoCSV()
+#wait to grab initial data
+#if coin_data_copy == []:
+	#display_number_white[0].set("Grabbing Data...Please Wait")
+
+
+
+
+thread = threading.Thread(target=DataGrabber_NoCSV)
+thread.start()
+
+
+
+
+while CoinList == []:
+	display_number_white[0].set("grabbing data...please wait")
+	root.lift()
+	root.focus()
+	root.update()
+	sleep(0.05)
+
+
 WriteToDB()
 
+display_number_white[0].set(" ")
+root.lift()
+root.focus()
+root.update()
 
-while True:
-	#print('display updated')
-	display_text.set(' '.join(coin_data_copy))
-	# text = text[1:]+text[0]
-	master.lift()
-	master.focus()
-	master.update()
-	sleep(1.0)
-	#CoinList = DataGrabber_NoCSV()
+
+
+
+
+root.mainloop()
+
+
 
